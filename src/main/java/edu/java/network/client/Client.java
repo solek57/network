e9ee923c6c09e3f070fb48d591ebb171af71a4ec
Suslov-DS <https://github.com/solek57/student-project>
2021-01-17 21:51:03 +0400

    multithread

--- src/main/java/edu/java/network/client/Client.java
+++ src/main/java/edu/java/network/client/Client.java
@@ -2,18 +2,33 @@ package edu.java.network.client;
 
 import java.io.*;
 import java.net.Socket;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        sendRequest();
+        for (int i = 0; i < 2; i++) {
+            int finalI = i;
+            new Thread(() -> {
+                try {
+                    System.out.println("started " + finalI + ": " + LocalDateTime.now());
+                    Thread.sleep(2000);
+                    String re = sendRequest(finalI);
+                    System.out.println("finish " + finalI + "@" + re + "@" + ": " + LocalDateTime.now());
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+
     }
 
-    private static void sendRequest() throws IOException {
-        Socket socket = new Socket("127.0.0.1", 25237);
+    private static String sendRequest(Integer i) throws IOException {
+        Socket socket = new Socket("127.0.0.1", 25238);
         BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-        String sb = "Dmitry ";
+        String sb = "Dmitry " + i;
 
         bw.write(sb);
         bw.newLine();
@@ -22,11 +37,11 @@ public class Client {
         String result = br.readLine();
         System.out.println(result);
 
-        System.out.println("result: " + result);
-
         socket.close();
         br.close();
         bw.close();
+
+        return result;
 
     }
 }
